297729373e95b99ba98bdd828693b1ef3933a08b
vivekprajapati2048 <vivekprajapati2048@gmail.com>
2022-04-19 18:33:12 +0530

    day-26

--- day-26-the-NATO-alphabet/dictionary_comprehension.py
+++ day-26-the-NATO-alphabet/dictionary_comprehension.py
@@ -25,11 +25,7 @@ weather_f = {day:(temp_c * 9/5) + 32 for (day,temp_c) in weather_c.items()}
 print(weather_f)
 # ---------------------------- #
 
-'''
-
 # loop through pandas df
 for (index, row) in student_df.iterrows():
     if row.studnet == 'Vivek':
         print(row.score)
-
-'''
