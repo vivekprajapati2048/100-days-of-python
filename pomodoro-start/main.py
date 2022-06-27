ad14b63b9651bc4abe52966c3bea45d7798c1db2
vivekprajapati2048 <vivekprajapati2048@gmail.com>
2022-06-27 22:31:41 +0530

    pomodoro ui setup

--- pomodoro-start/main.py
+++ pomodoro-start/main.py
@@ -1,4 +1,4 @@
-
+from tkinter import *
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
 RED = "#e7305b"
@@ -15,4 +15,30 @@ LONG_BREAK_MIN = 20
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- # 
 
+import time
+
+
 # ---------------------------- UI SETUP ------------------------------- #
+window = Tk()
+window.title('Pomodoro')
+window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = Label(text='Timer', fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50, "bold"))
+title_label.grid(column=1, row=0)
+
+canvas = Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
+tomato_img = PhotoImage(file='pomodoro-start\\tomato.png')
+canvas.create_image(100, 112, image=tomato_img)
+canvas.create_text(100, 130, text='00:00', fill='white', font=(FONT_NAME, 35, "bold"))
+canvas.grid(column=1, row=1)
+
+start_button = Button(text='Start', highlightthickness=0)
+start_button.grid(column=0, row=2)
+
+reset_button = Button(text='Reset', highlightthickness=0)
+reset_button.grid(column=2, row=2)
+
+check_marks = Label(text='✔', fg=GREEN, bg=YELLOW, font=(FONT_NAME, "20", "bold"))
+check_marks.grid(column=1, row=2)
+
+window.mainloop()
